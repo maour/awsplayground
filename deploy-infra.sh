b3a158aa7edd5172a49a02db11d27a2323396d46
Majid Ramezanpour <majid.ramezanpour@gmail.com>
2020-12-19 17:02:09 +0200

    added codebuild/deploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,18 +1,43 @@
-#!/bin/bash
+source aws_credentials.sh
 
-STACK_NAME=awsbootstrap 
+STACK_NAME=awsbootstrap
 REGION=eu-north-1
 CLI_PROFILE=awsbootstrap
 
-EC2_INSTANCE_TYPE=t2.micro 
+EC2_INSTANCE_TYPE=t3.micro
 
-# Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
-  --stack-name $STACK_NAME \
-  --template-file main.yml \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+    
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ===========" 
+aws cloudformation deploy \
+--region $REGION \
+--profile $CLI_PROFILE \
+--stack-name $STACK_NAME \
+--template-file main.yml \
+--no-fail-on-empty-changeset \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameter-overrides \
+EC2InstanceType=$EC2_INSTANCE_TYPE
+
+# If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --region $REGION \
+    --query "Exports[?Name=='InstanceEndpoint'].Value"
+fi
